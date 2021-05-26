901328c244f255a052c96f9acf8d6865972789ba
Bruno Natali <b010010010n@gmail.com>
2021-05-26 00:38:05 -0300

    removed unnecessary configs

--- backend/project/server.py
+++ backend/project/server.py
@@ -18,9 +18,6 @@ basedir = Path(__file__).resolve().parent
 # CONFIGS
 DEBUG = True
 DATABASE = "app.db"
-USERNAME = "admin"
-PASSWORD = "admin"
-SECRET_KEY = "bnatali" # Hey, it`s me
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(basedir).joinpath(DATABASE)}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
