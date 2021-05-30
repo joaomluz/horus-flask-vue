cbef3066b8271f5588ddb0a450b071b8e492cb44
Bruno Natali <b010010010n@gmail.com>
2021-05-30 14:36:12 -0300

    Implemented alert to display action results

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,15 @@
+<!--
+    This template will be added here to show how other componets may integrate page
+-->
+<template>
+  <div>
+    <b-alert show>{{ message }}</b-alert>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
